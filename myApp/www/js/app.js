09ff610da33753d48cbb4c50b10b401ed6b05c6f
Danny McGrory <dannyalaska@mac.com>
2016-02-05 16:39:43 -0500

    finished tutorial

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -7,7 +7,7 @@ angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -17,8 +17,108 @@ angular.module('starter', ['ionic'])
       // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
-    if(window.StatusBar) {
+
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('tabs', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html',
+  })
+
+  .state('tabs.home', {
+    url: '/home',
+    views: {
+      'home-tab': {
+        templateUrl: 'templates/home.html',
+      },
+    },
+  })
+
+  .state('tabs.list', {
+    url: '/list',
+    views: {
+      'list-tab': {
+        templateUrl: 'templates/list.html',
+        controller: 'ListController',
+      },
+    },
+  })
+
+  .state('tabs.detail', {
+    url: '/list/:aId',
+    views: {
+      'list-tab': {
+        templateUrl: 'templates/detail.html',
+        controller: 'ListController',
+      },
+    },
+  })
+
+  .state('tabs.calendar', {
+    url: '/calendar',
+    views: {
+      'calendar-tab': {
+        templateUrl: 'templates/calendar.html',
+        controller: 'CalendarController',
+      },
+    },
+  })
+
+  $urlRouterProvider.otherwise('/tab/home');
+})
+
+.controller('CalendarController', ['$scope', '$http', '$state', function($scope, $http, $state) {
+  $http.get('js/data.json').success(function(data) {
+    $scope.calendar = data.calendar;
+
+    $scope.onItemDelete = function(dayIndex, item) {
+      $scope.calendar[dayIndex].schedule.splice($scope.calendar[dayIndex].schedule.indexOf(item), 1);
+    };
+
+    $scope.doRefresh = function() {
+      $http.get('js/data.json').success(function(data) {
+      $scope.calendar = data.calendar;
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+    };
+
+    $scope.toggleStar = function(item) {
+      item.star =  !item.star;
+    };
+  });
+}])
+
+.controller('ListController', ['$scope', '$http', '$state', function($scope, $http, $state) {
+  $http.get('js/data.json').success(function(data) {
+    $scope.artists = data.artists;
+    $scope.whichartist = $state.params.aId;
+    $scope.data = { showDelete: false, showReorder: false};
+
+    $scope.onItemDelete = function(item) {
+      $scope.artists.splice($scope.artists.indexOf(item), 1);
+    };
+
+    $scope.doRefresh = function() {
+      $http.get('js/data.json').success(function(data) {
+      $scope.artists = data;
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+    };
+
+    $scope.toggleStar = function(item) {
+      item.star =  !item.star;
+    };
+
+    $scope.moveItem = function(item, fromIndex, toIndex) {
+      $scope.artists.splice(fromIndex, 1);
+      $scope.artists.splice(toIndex, 0, item);
+    };
+  });
+}]);
